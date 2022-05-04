4915bcf54de68cbc7e1d4041e51f045e65d30280
mikulas <heinz.mikulas@gmail.com>
2022-05-04 02:14:03 +0200

    komentare, odstraneni imshow()

--- main.py
+++ main.py
@@ -36,8 +36,9 @@ def otevrit(image):
     return eroded
 
 
-#
+
 def alterImage(frame, proportions=(500, 400)):
+    """returns original image, hsv variant and greyscale variant"""
     frame = cv2.resize(frame, proportions)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -52,7 +53,7 @@ def getColorAndPositionFromUserCallback(event, x, y, flags, param):
         detectedColor = frame[y, x]
         position = (x, y)
 
-
+# nepouzito
 def getMaskFromContours(grayImage, position, clrd):
     clrd = clrd.copy()
     obr = grayImage.copy()
@@ -61,17 +62,8 @@ def getMaskFromContours(grayImage, position, clrd):
     kernel = np.ones((2, 2), np.uint8)
     bluredSharp = cv2.bilateralFilter(sharp, 9, 75, 75)
     edgesSharp = cv2.Canny(bluredSharp, 120, 255, 1)
-    # eroze a pak diletace = otevreni, oddeleni od sebe blizkych objektu
-    # eroded = cv2.erode(edgesSharp, kernel)
     diluted = cv2.dilate(edgesSharp, kernel, iterations=1)
-    # cv2.imshow("edges", diluted)
-    # ret, thresh = cv2.threshold(gray, 127, 255, 0)
-    # contours, hierarchy = cv2.findContours(blur1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # contours, hierarchy = cv2.findContours(
-    #     diluted, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours, hierarchy = cv2.findContours(diluted, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    # cv2.drawContours(clrd, contours, -1, (0, 255, 255), 3)
-    # cv2.imshow("contoured", clrd)
     template = np.zeros(grayImage.shape)
     clrd = cv2.circle(clrd, position, radius=3, color=(255, 0, 255), thickness=-1)
     if contours is not None:
@@ -86,24 +78,15 @@ def getMaskFromContours(grayImage, position, clrd):
                     template, [cnt], -1, color=(255, 255, 255), thickness=-1)
             else:
                 cv2.drawContours(clrd, [cnt], contourIdx=-1, color=(0, 0, 125), thickness=-1)
-    # in case of error of stream
-    # cv2.imshow("CONTOURSSS", clrd)
-    # TODO prekryvani contours hran ruznych hran je potreba jeste optimalizovat, mozna odecist prvni image? to by mohlo odstranit hranu podlozky
-    # cv2.imshow("teamplate", template)
     return np.uint8(template)
 
 
 """main program"""
 # read stream
-# images = ['nohy.png', 'kostka.png']
-# img = cv2.imread('./imgs/'+images[1])
-# mask = getMaskFromContours(img, (430, 445))
-videos = ['./edited/18.mp4', './edited/Jur_OK_1.mp4', './edited/kostka2.mp4']
+videos = ['./edited/2.mp4', './edited/Jur_OK_1.mp4', './edited/kostka2.mp4']  # examples of videos
 tolerance_horni = 1000
-tolerance_dolni = 50
+tolerance_dolni = 10
 cap = cv2.VideoCapture(videos[0])
-
-# model = keras.models.load_model('./models/myModels/transferModelV2', custom_objects={'KerasLayer': hub.KerasLayer})
 
 if not cap.isOpened():
     print("cannot read video input")
@@ -115,7 +98,9 @@ frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 print("frame_width", frame_width)
 print("frame_height", frame_height)
 
-"""get color to detect"""
+"""get color from image to detect
+right arrow to next frame, esq to continue detection
+"""
 cv2.namedWindow('getColor')
 cv2.setMouseCallback("getColor", getColorAndPositionFromUserCallback)
 detectedColor, position = None, None
@@ -152,7 +137,6 @@ print("upperColor", upperColor)
 ret, frame = cap.read()
 frame = cv2.bilateralFilter(frame, 15, 75, 75)  # snizit d=velikost filtru, pokud nebude stihat
 frame, hsv, _ = alterImage(frame, finalWidth)
-cv2.imshow("hsv", hsv)
 mask = cv2.inRange(hsv, lowColor, upperColor)
 prevMasked = cv2.bitwise_and(frame, frame, mask=mask)
 """MAIN LOOP"""
@@ -162,18 +146,18 @@ while True:
     ret, cFrame = cap.read()
     if not ret:
         break
+    #     vyhladit plochy
     cFrame = cv2.bilateralFilter(cFrame, 5, 75, 75)
+    # prevest do hsv a grayscale
     cFrame, hsv, gray = alterImage(cFrame, finalWidth)
+    # nechat jen pixely v ramci mezich hsv barvy
     hueMask = cv2.inRange(hsv, lowColor, upperColor)
     masked = cv2.bitwise_and(cFrame, cFrame, mask=hueMask)
-    # contourMask = getMaskFromContours(gray, position, cFrame)
-    # masked2 = cv2.bitwise_and(cFrame, cFrame, mask=contourMask)
     """Detect errors here!!!"""
+    # rozdil dvou poslednich snimku
     diff = cv2.absdiff(prevMasked, masked)
     h, s, grayFromHUE = cv2.split(diff)
-    cv2.imshow("gray manually", diff)
     blur = cv2.GaussianBlur(grayFromHUE, (5, 5), 0)
-    cv2.imshow("gauss", blur)
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
     dilated = cv2.dilate(thresh, None, iterations=3)
     contours, _ = cv2.findContours(
